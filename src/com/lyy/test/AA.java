d41b4ee220d74b9d543b1d244b6498b1e0c5e258
Your Name <2047419621@qq.com>
2019-08-28 19:41:29 +0800

    添加了一个包类方法

---
+++ src/com/lyy/test/AA.java
@@ -0,0 +1,21 @@
+
+package com.lyy.test;  
+ 
+/** 
+ *
+ * @author: Lyy
+ * @Date: 2019年8月28日 下午7:36:28
+ * @package: com.lyy.test.AA.java
+ */
+public class AA {
+	public static void main(String[] args) {
+		AA aa = new AA();
+		aa.aa();
+	}
+	
+	public void aa() {
+		System.err.println("修改内容");
+	}
+
+}
+ 
